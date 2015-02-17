4a846d801705317b9d9042855df6e2fa79b8b01b
hubertbrosseau <hubert.brosseau@gmail.com>
2015-02-17 16:07:09 +0100

    added figaro

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,16 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
+      before_action :configure_permitted_parameters
 
     protected
 
     def update_resource(resource, params)
       resource.update_without_password(params)
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:account_update) << [:phonenumber, :firstname, :lastname, :picture]
+      devise_parameter_sanitizer.for(:sign_up)        << [:phonenumber, :firstname, :lastname]
     end
   end
 end
