f196cbeb425dc836468f0b1ce8cfb18d3e5d7f69
hubertbrosseau <hubert.brosseau@gmail.com>
2015-02-18 13:27:47 +0100

    mandrill avance

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,6 +17,7 @@ class OrdersController < ApplicationController
         format.json { render :show, status: :created, location: @order }
         @order.booked = true
         @order.save
+        send_owner_validation_email
       else
         format.html { render :new }
         format.json { render json: @order.errors, status: :unprocessable_entity }
@@ -48,5 +49,11 @@ class OrdersController < ApplicationController
 
   def order_params
    params.require(:order).permit(:date_begin, :date_end, :booked)
+  end
+
+  def send_owner_validation_email
+    user = User.find(@flat.user_id)
+    Usermailer.owner_validation(user).deliver
+    raise
   end
 end
