7f0845da97060395300fc1fb5044735a7b4d5f64
jbmoutout <jbmoutout@gmail.com>
2015-02-18 16:04:48 +0100

    order#index

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,16 @@ class Order < ActiveRecord::Base
   belongs_to :flat
   belongs_to :user
 
+  def accepted?
+    self.accepted == true
+  end
+
+  def pending?
+    self.accepted == nil
+  end
+
+  def refused?
+    self.accepted == false
+  end
+
 end
