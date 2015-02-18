27248a33ea6314e4ffb45b58847c9c29c2bf21be
jeremie <marichal.jeremie@gmail.com>
2015-02-18 12:28:51 +0100

    it is so beautifull

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,5 @@
+module UsersHelper
+  def picture_for(user)
+    user.picture_file_name ? image_tag(user.picture.url(:thumb), class: "img-avatar") : image_tag("Airbnb.jpg", class: "img-avatar")
+  end
+end
